b964e9ab29c0f623e2c5467ec64d2450537fe911
Nick Remley <remleynick@yahoo.com>
2022-12-03 10:40:31 -0700

    Week 1 Workshop Assignment

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
-import React from 'react';
-import { Container, Navbar, NavbarBrand } from 'reactstrap';
-import NucampLogo from './app/assets/img/logo.png';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import CampsitesList from './features/campsites/CampsitesList.js';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-        <Navbar dark color='primary' sticky='top' expand='mad'>
-          <Container>
-            <NavbarBrand href='/' >
-              <img src={NucampLogo} alt='nucamp logo' />
-            </NavbarBrand>
-          </Container>
-        </Navbar>
-        I'm ready for workshop!
+        <Header />
+        <CampsitesList />
+        <Footer />
     </div>
   );
 }
